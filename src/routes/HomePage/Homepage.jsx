b866cbb30a20b21aad5b426cd7a074a89a3c7646
ZahaAnass <zahaanass277@gmail.com>
2025-09-14 21:34:05 +0100

    add all pages

---
+++ src/routes/HomePage/Homepage.jsx
@@ -0,0 +1,6 @@
+
+export const HomePage = () => {
+    return (
+        <div className="homepage">HomePage</div>
+    )
+}
---
+++ src/routes/SignIn/SignIn.jsx
@@ -0,0 +1,6 @@
+
+export const SignIn = () => {
+    return (
+        <div className="signin">SignIn</div>
+    )
+}
