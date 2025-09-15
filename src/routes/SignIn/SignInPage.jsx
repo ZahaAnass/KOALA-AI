71d69924a88f097bb147aa8aeaa1dcfe155cb3aa
ZahaAnass <zahaanass277@gmail.com>
2025-09-16 00:11:27 +0100

    using clerk in the signup page

--- src/routes/SignIn/SignInPage.jsx
+++ src/routes/SignIn/SignInPage.jsx
@@ -2,8 +2,8 @@ import { SignIn } from "@clerk/clerk-react"
 
 export const SignInPage = () => {
     return (
-        <div className="signin">
-            <SignIn path="/sign-in" />
+        <div className="signInPage h-full flex items-center justify-center">
+            <SignIn path="/sign-in" signUpUrl="/sign-up"/>
         </div>
     )
 }
