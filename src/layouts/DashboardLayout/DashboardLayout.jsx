63220ad1f3d5f29c85bf2b73b2115e435837f8f0
ZahaAnass <zahaanass277@gmail.com>
2025-09-19 22:17:52 +0100

    add chat list in the menu

--- src/layouts/DashboardLayout/DashboardLayout.jsx
+++ src/layouts/DashboardLayout/DashboardLayout.jsx
@@ -1,6 +1,7 @@
 import { useAuth } from "@clerk/clerk-react";
 import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
+import ChatList from "../../componenets/chatList/ChatList";
 
 export const DashboardLayout = () => {
 
@@ -16,9 +17,11 @@ export const DashboardLayout = () => {
     if (!isLoaded) return "Loading ...";
 
     return (
-        <div className="dashboardLayout">
-            <div className="menu">MENU</div>
-            <div className="content">
+        <div className="dashboardLayout flex gap-12 pt-5 h-full">
+            <div className="menu flex-1">
+                <ChatList />
+            </div>
+            <div className="content flex-4 bg-[#12101b]">
                 <Outlet />
             </div>
         </div>
