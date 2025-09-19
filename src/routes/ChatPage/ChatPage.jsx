67e6eec6c822f181702d44e82d568f11e49b721f
ZahaAnass <zahaanass277@gmail.com>
2025-09-19 22:16:36 +0100

    improve Chat page

--- src/routes/ChatPage/ChatPage.jsx
+++ src/routes/ChatPage/ChatPage.jsx
@@ -1,6 +1,46 @@
+import { useRef, useEffect } from "react";
+import NewPompt from "../../componenets/NewPompt/NewPompt";
 
 export const ChatPage = () => {
+
+    const endRef = useRef(null);
+
+    useEffect(() => {
+        endRef.current.scrollIntoView({ behavior: "smooth" });
+    }, [])
+
     return (
-        <div className="chatpage">ChatPage</div>
+        <div className="chatPage h-full flex flex-col items-center">
+            <div className="wrapper flex-1 w-full flex justify-center">
+                <div className="chat w-1/2 flex flex-col">
+                    <div className="message">Test message from ai</div>
+                    <div className="message user">Test message from user</div>
+                    <div className="message">Test message from ai</div>
+                    <div className="message user">Test message from user</div>
+                    <div className="message">Test message from ai</div>
+                    <div className="message user">Test message from user</div>
+                    <div className="message">Test message from ai</div>
+                    <div className="message user">Test message from user</div>
+                    <div className="message">Test message from ai</div>
+                    <div className="message user">Test message from user</div>
+                    <div className="message">Test message from ai</div>
+                    <div className="message user">Test message from user</div>
+                    <div className="message">Test message from ai</div>
+                    <div className="message user">Test message from user</div>
+                    <div className="message">Test message from ai</div>
+                    <div className="message user">Test message from user</div>
+                    <div className="message">Test message from ai</div>
+                    <div className="message user">Test message from user</div>
+                    <div className="message">Test message from ai</div>
+                    <div className="message user">Test message from user</div>
+                    <div className="message">Test message from ai</div>
+                    <div className="message user">Test message from user</div>
+                    <div className="message">Test message from ai</div>
+                    <div className="message user">Test message from user</div>
+                    <NewPompt />
+                    <div ref={endRef}></div>
+                </div>
+            </div>
+        </div>
     )
 }
