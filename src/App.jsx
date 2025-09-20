b1238de333d4cb987765349ed470d327a1e9df23
ZahaAnass <zahaanass277@gmail.com>
2025-09-20 11:33:35 +0100

    move files inside client folder

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-
-const App = () => {
-  return (
-    <div className="text-amber-300 dark:text-red-500 ">Hello World</div>
-  )
-}
-
-export default App
--- src/routes/SignIn/SignInPage.jsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/clerk-react"
-
-export const SignInPage = () => {
-    return (
-        <div className="signInPage h-full flex items-center justify-center">
-            <SignIn path="/sign-in" signUpUrl="/sign-up" forceRedirectUrl="/dashboard"/>
-        </div>
-    )
-}
