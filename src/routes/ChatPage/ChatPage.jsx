399b7bb241d23455a3811e1f9b18b39ccb074362
ZahaAnass <zahaanass277@gmail.com>
2025-09-20 11:24:24 +0100

    fix the scroll behavior in chat page

--- src/routes/ChatPage/ChatPage.jsx
+++ src/routes/ChatPage/ChatPage.jsx
@@ -1,17 +1,10 @@
-import { useRef, useEffect } from "react";
 import NewPompt from "../../componenets/NewPompt/NewPompt";
 
 export const ChatPage = () => {
 
-    const endRef = useRef(null);
-
-    useEffect(() => {
-        endRef.current.scrollIntoView({ behavior: "smooth" });
-    }, [])
-
     return (
-        <div className="chatPage h-full flex flex-col items-center">
-            <div className="wrapper flex-1 w-full flex justify-center">
+        <div className="chatPage h-full flex flex-col items-center relative">
+            <div className="wrapper flex-1 w-full flex justify-center overflow-scroll">
                 <div className="chat w-1/2 flex flex-col">
                     <div className="message">Test message from ai</div>
                     <div className="message user">Test message from user</div>
@@ -37,8 +30,11 @@ export const ChatPage = () => {
                     <div className="message user">Test message from user</div>
                     <div className="message">Test message from ai</div>
                     <div className="message user">Test message from user</div>
+                    <div className="message">Test message from ai</div>
+                    <div className="message user">Test message from user</div>
+                    <div className="message">Test message from ai</div>
+                    <div className="message user">Test message from user</div>
                     <NewPompt />
-                    <div ref={endRef}></div>
                 </div>
             </div>
         </div>
