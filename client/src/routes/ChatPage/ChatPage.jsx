0339ecd9e22afd67b05ec6f6c8290baa793b33a7
ZahaAnass <zahaanass277@gmail.com>
2025-09-22 11:51:07 +0100

    add ChatPage.css

--- client/src/routes/ChatPage/ChatPage.jsx
+++ client/src/routes/ChatPage/ChatPage.jsx
@@ -1,11 +1,12 @@
 import NewPompt from "../../componenets/NewPompt/NewPompt";
+import "./ChatPage.css"
 
 export const ChatPage = () => {
 
     return (
         <div className="chatPage h-full flex flex-col items-center relative">
             <div className="wrapper flex-1 w-full flex justify-center overflow-scroll">
-                <div className="chat w-1/2 flex flex-col">
+                <div className="chat w-1/2 flex flex-col gap-5">
                     <div className="message">Test message from ai</div>
                     <div className="message user">Test message from user</div>
                     <div className="message">Test message from ai</div>
@@ -33,7 +34,6 @@ export const ChatPage = () => {
                     <div className="message">Test message from ai</div>
                     <div className="message user">Test message from user</div>
                     <div className="message">Test message from ai</div>
-                    <div className="message user">Test message from user</div>
                     <NewPompt />
                 </div>
             </div>
