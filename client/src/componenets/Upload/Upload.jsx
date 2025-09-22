46088f09c3993297c6939b8c43fdf6c195fbd393
ZahaAnass <zahaanass277@gmail.com>
2025-09-22 14:25:35 +0100

    update onUploadStart to handle file upload

--- client/src/componenets/Upload/Upload.jsx
+++ client/src/componenets/Upload/Upload.jsx
@@ -47,8 +47,19 @@ export const Upload = ({ setImg }) => {
     };
     
     const onUploadStart = evt => {
-        console.log("Start", evt);
-        setImg(prevImg => ({ ...prevImg, isLoading: true }))
+        const file = evt.target.files[0]
+
+        const reader = new FileReader()
+        reader.onloadend = () => {
+            setImg(prevImg => ({ ...prevImg, isLoading: true, aiData: {
+                inlineData:{
+                    data: reader.result.split(",")[1],
+                    mimeType: file.type,
+                },
+            } 
+        }))
+        }
+        reader.readAsDataURL(file)
     };
 
     return (
