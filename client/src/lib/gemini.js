2cf28d9ed7cba6dfb2d3b238ae9447db4bc0790d
ZahaAnass <zahaanass277@gmail.com>
2025-10-03 13:36:46 +0100

    finishing gemini api

--- client/src/lib/gemini.js
+++ client/src/lib/gemini.js
@@ -1,27 +1,57 @@
 import { GoogleGenAI } from "@google/genai";
 
-const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_PUBLIC_KEY });
+const safetySettings = [
+    {
+        category: "HARM_CATEGORY_HARASSMENT",
+        threshold: "BLOCK_LOW_AND_ABOVE",
+    },
+    {
+        category: "HARM_CATEGORY_HATE_SPEECH",
+        threshold: "BLOCK_LOW_AND_ABOVE",
+    },
+];
 
-async function SendPrompt({ image = null, prompt = "" }) {
-  const response = await ai.models.generateContentStream({
-    model: "gemini-1.5-flash",
-    contents: "Explain how AI works",
-    safetySettings: [
-        {
-            maxtoken: 100
-        }
-    ]
-  });
+const genAI = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_PUBLIC_KEY });
 
-  let text = ""
-  for await (const chunk of response) {
-    text += chunk.text;
-    console.log(chunk.text);
-  }
-
-  console.log(text.text);
-  return text;
+const config = {
+    safetySettings,
+    thinkingConfig: {
+        thinkingBudget: 0, // Disables thinking
+    }
 }
 
-export default SendPrompt;
+async function SendPrompt({ image = null, prompt = "" }) {
 
+    let contents = [];
+
+    if(image){
+        contents.push({ 
+            inlineData: {
+                mimeType: image.mimeType,
+                data: image.data,
+            }
+        })
+    }
+
+    if(prompt && prompt.trim()){
+        contents.push({ 
+            text: prompt 
+        })
+    }
+
+    // console.log("Sending Content", contents)
+
+    const response = await genAI.models.generateContent({
+        model: "gemini-2.5-flash",
+        config: config,
+        contents: contents
+    });
+    // console.log("Response", response.text)
+    return response;
+}
+
+export {
+    SendPrompt,
+    genAI,
+    config
+}
