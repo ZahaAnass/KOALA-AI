99c6aff1ae7a6ea8a178991dcb89a271b25da9d7
ZahaAnass <zahaanass277@gmail.com>
2025-10-06 22:38:55 +0100

    add queryclient

--- client/src/layouts/RootLayout/RootLayout.jsx
+++ client/src/layouts/RootLayout/RootLayout.jsx
@@ -1,5 +1,6 @@
 import { Link, Outlet } from "react-router-dom"
 import { ClerkProvider, SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/clerk-react"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export const RootLayout = () => {
 
@@ -9,27 +10,31 @@ export const RootLayout = () => {
         throw new Error('Missing Publishable Key')
     }
 
+    const queryClient = new QueryClient()
+
     return (
         <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-            <div className="rootLayout py-4 px-12 h-screen flex flex-col">
-                <header className="flex items-center justify-between">
-                    <Link to="/" className="logo flex items-center font-bold gap-2">
-                        <img src="/logo.png" alt="logo" className="w-12 h-12"/>
-                        <span>KOALA AI</span>
-                    </Link>
-                    <div className="user">
-                        <SignedOut>
-                            <SignInButton />
-                        </SignedOut>
-                        <SignedIn>
-                            <UserButton />
-                        </SignedIn>
-                    </div>
-                </header>
-                <main className="overflow-hidden flex-1">
-                    <Outlet />
-                </main>
-            </div>
+            <QueryClientProvider client={queryClient}>
+                <div className="rootLayout py-4 px-12 h-screen flex flex-col">
+                    <header className="flex items-center justify-between">
+                        <Link to="/" className="logo flex items-center font-bold gap-2">
+                            <img src="/logo.png" alt="logo" className="w-12 h-12"/>
+                            <span>KOALA AI</span>
+                        </Link>
+                        <div className="user">
+                            <SignedOut>
+                                <SignInButton />
+                            </SignedOut>
+                            <SignedIn>
+                                <UserButton />
+                            </SignedIn>
+                        </div>
+                    </header>
+                    <main className="overflow-hidden flex-1">
+                        <Outlet />
+                    </main>
+                </div>
+            </QueryClientProvider>
         </ClerkProvider>
     )
 }
