ca6b6af3ea45bfdbe0df6f798fe2c5c43d1140b0
ZahaAnass <zahaanass277@gmail.com>
2025-10-07 11:12:59 +0100

    use react query to fetch real data insted of hard coded one

--- client/src/routes/ChatPage/ChatPage.jsx
+++ client/src/routes/ChatPage/ChatPage.jsx
@@ -1,39 +1,68 @@
 import NewPompt from "../../componenets/NewPompt/NewPompt";
+import { useQuery } from "@tanstack/react-query"
+import { useAuth } from "@clerk/clerk-react"
 import "./ChatPage.css"
+import { useLocation } from "react-router-dom";
+import Markdown from "react-markdown";
+import { IKImage } from "imagekitio-react";
 
 export const ChatPage = () => {
+
+    const path = useLocation().pathname
+    const chatId = path.split("/").pop()
+
+    const { getToken } = useAuth()
+
+    const { isPending, error, data } = useQuery({
+        queryKey: ['singleChat', chatId],
+        queryFn: async () => {
+            const token = await getToken()
+
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/chats/${chatId}`,
+                {
+                    method: 'GET',
+                    credentials: 'include',
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                        'Content-Type': 'application/json'
+                    }
+                }
+            )
+
+            if (!response.ok) {
+                console.log(response)
+            }
+
+            return await response.json()
+        }
+    })
 
     return (
         <div className="chatPage h-full flex flex-col items-center relative">
             <div className="wrapper flex-1 w-full flex justify-center overflow-scroll">
                 <div className="chat w-1/2 flex flex-col gap-5">
-                    <div className="message">Test message from ai</div>
-                    <div className="message user">Test message from user</div>
-                    <div className="message">Test message from ai</div>
-                    <div className="message user">Test message from user</div>
-                    <div className="message">Test message from ai</div>
-                    <div className="message user">Test message from user</div>
-                    <div className="message">Test message from ai</div>
-                    <div className="message user">Test message from user</div>
-                    <div className="message">Test message from ai</div>
-                    <div className="message user">Test message from user</div>
-                    <div className="message">Test message from ai</div>
-                    <div className="message user">Test message from user</div>
-                    <div className="message">Test message from ai</div>
-                    <div className="message user">Test message from user</div>
-                    <div className="message">Test message from ai</div>
-                    <div className="message user">Test message from user</div>
-                    <div className="message">Test message from ai</div>
-                    <div className="message user">Test message from user</div>
-                    <div className="message">Test message from ai</div>
-                    <div className="message user">Test message from user</div>
-                    <div className="message">Test message from ai</div>
-                    <div className="message user">Test message from user</div>
-                    <div className="message">Test message from ai</div>
-                    <div className="message user">Test message from user</div>
-                    <div className="message">Test message from ai</div>
-                    <div className="message user">Test message from user</div>
-                    <div className="message">Test message from ai</div>
+                    {/* <div className="message">Test message from ai</div>
+                    <div className="message user">Test message from user</div> */}
+
+                {isPending ? <p>Loading...</p> : error ? <p>Somthing went wrong!</p> : data?.history?.map((message, index) => (
+                    <>
+                        {message.img && (
+                            <IKImage
+                                urlEndpoint={import.meta.env.VITE_IMAGE_KIT_END_POINT}
+                                path={message.img}
+                                height="300"
+                                width="400"
+                                transformation={[{ height: "300", width: "400" }]}
+                                loading="lazy"
+                                lqip={{ active: true , quality: 20}}
+                            />
+                        )}
+                        <div key={index} className={`message ${message.role === 'user' ? 'user' : 'ai'}`}>
+                            <Markdown>{message.parts[0].text}</Markdown>
+                        </div>
+                    </>
+                ))}
+
                     <NewPompt />
                 </div>
             </div>
