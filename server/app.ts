7a98c85563ed26e1991ae636cc6d1ccf7d7c747d
ZahaAnass <zahaanass277@gmail.com>
2025-10-07 18:24:03 +0100

    refactor index.js

---
+++ server/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import chatRoutes from "./routes/chatRoutes.js";
+import userChatsRoutes from "./routes/userChatsRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import notFound from "./middleware/not-found.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(express.json());
+
+app.get("/", (_, res) => res.send("Backend is running 🚀"));
+
+app.use("/api/chats", chatRoutes);
+app.use("/api/userchats", userChatsRoutes);
+app.use("/api/upload", uploadRoutes);
+
+app.use(notFound);
+
+export default app;
---
+++ server/server.ts
@@ -0,0 +1,13 @@
+import app from "./app.js";
+import connectDB from "./db/connect.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, async () => {
+    await connectDB(process.env.MONGO_URI as string);
+    console.log("Connected to MongoDB");
+    console.log(`Server is running on port ${port}`);
+});
