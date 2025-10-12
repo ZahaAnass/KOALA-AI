0828e1f88a66b82143b2dc67a0c5d03771bffd32
ZahaAnass <zahaanass277@gmail.com>
2025-10-12 18:07:54 +0100

    imrpove dashboardLayout and for sidebar

--- client/src/layouts/DashboardLayout/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout/DashboardLayout.jsx
@@ -2,7 +2,7 @@ import { useAuth } from "@clerk/clerk-react";
 import { useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import ChatList from "../../componenets/chatList/ChatList";
-import { MedalIcon, BotIcon } from "lucide-react";
+import { X, TextAlignJustify } from "lucide-react";
 
 export const DashboardLayout = () => {
 
@@ -21,11 +21,11 @@ export const DashboardLayout = () => {
 
     return (
         <div className="dashboardLayout flex gap-12 pt-5 h-full relative">
-            <div className={`menu ${isOpen ? "flex-1" : "flex-0"}`}>
+            <div className={`menu ${isOpen ? "max-w-[250px]" : "w-0"} transition-all duration-500 ease-in-out`}>
                 <div className={`flex flex-row ${isOpen ? "w-full" : "w-0"}`}>
                     {isOpen && <ChatList />}
-                    <button onClick={() => setIsOpen(!isOpen)} className="menuButton flex mt-3">
-                        {isOpen ? <BotIcon className="w-6 h-6" /> : <MedalIcon className="w-6 h-6" />}
+                    <button onClick={() => setIsOpen(!isOpen)} className="menuButton flex h-11 hover:bg-gray-800 rounded-[10px] p-2">
+                        {isOpen ? <X className=" w-6 h-6" /> : <TextAlignJustify className="w-6 h-6" />}
                     </button>
                 </div>
             </div>
