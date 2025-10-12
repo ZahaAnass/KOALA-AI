032309dc2e105f12062414cde1921a6b2fe696ff
ZahaAnass <zahaanass277@gmail.com>
2025-10-12 15:28:48 +0100

    add sidebar toggle

--- client/src/layouts/DashboardLayout/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout/DashboardLayout.jsx
@@ -1,12 +1,15 @@
 import { useAuth } from "@clerk/clerk-react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import ChatList from "../../componenets/chatList/ChatList";
+import { MedalIcon, BotIcon } from "lucide-react";
 
 export const DashboardLayout = () => {
 
     const {userId, isLoaded} = useAuth();
     const navigate = useNavigate();
+
+    const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         if (isLoaded && !userId) {
@@ -17,9 +20,14 @@ export const DashboardLayout = () => {
     if (!isLoaded) return "Loading ...";
 
     return (
-        <div className="dashboardLayout flex gap-12 pt-5 h-full">
-            <div className="menu flex-1">
-                <ChatList />
+        <div className="dashboardLayout flex gap-12 pt-5 h-full relative">
+            <div className={`menu ${isOpen ? "flex-1" : "flex-0"}`}>
+                <div className={`flex flex-row ${isOpen ? "w-full" : "w-0"}`}>
+                    {isOpen && <ChatList />}
+                    <button onClick={() => setIsOpen(!isOpen)} className="menuButton flex mt-3">
+                        {isOpen ? <BotIcon className="w-6 h-6" /> : <MedalIcon className="w-6 h-6" />}
+                    </button>
+                </div>
             </div>
             <div className="content flex-4 bg-[#12101b]">
                 <Outlet />
